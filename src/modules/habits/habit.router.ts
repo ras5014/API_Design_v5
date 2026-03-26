7161591d14d0092fc6763d68779c1c02f1c40c92
Ramananda Samantaray <ramanandasamantaray@gmail.com>
2026-03-26 23:33:20 +0530

    feat: refactor getUserHabits route to use controller function

--- src/modules/habits/habit.router.ts
+++ src/modules/habits/habit.router.ts
@@ -2,16 +2,14 @@ import { Router } from "express";
 import { authenticateToken } from "../../middlewares/auth.ts";
 import { validateBody } from "../../middlewares/validation.ts";
 import { createHabitSchema } from "./habit.types.ts";
-import { createHabit } from "./habit.controller.ts";
+import { createHabit, getUserHabits } from "./habit.controller.ts";
 
 const router = Router();
 
 // Route Level Authentication Middleware
 router.use(authenticateToken);
 
-router.get("/", (req, res) => {
-  res.json({ message: "Get all habits" });
-});
+router.get("/", getUserHabits);
 
 router.post("/", validateBody(createHabitSchema), createHabit);
 
